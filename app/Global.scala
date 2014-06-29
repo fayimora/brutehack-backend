ae65c4e0665318ca70d85881714f2bc8f2efbec7
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-06-29 22:13:36 +0100

    Drop all tables and re-create them onStart

--- app/Global.scala
+++ app/Global.scala
@@ -4,7 +4,7 @@ import models.database._
 import play.api.db.DB
 import play.api.Application
 import play.api.Play.current
-
+import scala.slick.jdbc.meta.MTable
 import utils.MyPostgresDriver.simple._
 
 object Global extends GlobalSettings {
@@ -15,7 +15,11 @@ object Global extends GlobalSettings {
     val problems = TableQuery[Problems]
     database withSession { implicit session =>
       val ddl = (contests.ddl ++ problems.ddl)
-      ddl.drop
+      val dbs = Map("CONTESTS" -> contests.ddl, "PROBLEMS" -> problems.ddl)
+      dbs foreach { db =>
+        if(MTable.getTables(db._1).list.nonEmpty)
+          db._2.drop
+      }
       ddl.create
     }
   }
