c6217b929eeaa54ee44ce63cd818114ea70bf896
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-06-29 18:43:43 +0100

    Add Problems table

---
+++ app/models/Problem.scala
@@ -0,0 +1,13 @@
+package models
+
+import java.sql.Timestamp
+
+/*  Input and Output fields are simply stubs for now */
+case class Problem(id: Long,
+                   createdAt: Timestamp,
+                   updatedAt: Timestamp,
+                   author: String,
+                   title: String,
+                   description: String,
+                   input: String,
+                   output: String)
---
+++ app/models/database/Problems.scala
@@ -0,0 +1,19 @@
+package models.database
+
+import models.Problem
+import scala.slick.driver.PostgresDriver.simple._
+import java.sql.Timestamp
+
+class Problems(tag: Tag) extends Table[Problem](tag, "PROBLEMS") {
+  def id = column[Long]("PROBLEM_ID", O.PrimaryKey, O.AutoInc, O.NotNull)
+  def createdAt = column[Timestamp]("CREATED_AT", O.NotNull)
+  def updatedAt = column[Timestamp]("UPDATED_AT", O.NotNull)
+  def author = column[String]("AUTHOR", O.NotNull)
+  def title = column[String]("TITLE", O.NotNull)
+  def description = column[String]("DESCRIPTION", O.NotNull, O.DBType("CLOB"))
+  def input = column[String]("INPUT", O.NotNull, O.DBType("CLOB"))
+  def output = column[String]("OUTPUT", O.NotNull, O.DBType("CLOB"))
+
+  def * = (id, createdAt, updatedAt, author, title, description, input, output) <> (Problem.tupled, Problem.unapply)
+}
+
