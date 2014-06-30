61fe46149d5735b0705065bea4cc1e131a7d695d
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-06-30 11:31:50 +0100

    Insert some contests into the db onStart

--- app/Global.scala
+++ app/Global.scala
@@ -1,11 +1,13 @@
 import play.api.GlobalSettings
 
+import models._
 import models.database._
 import play.api.db.DB
 import play.api.Application
 import play.api.Play.current
 import scala.slick.jdbc.meta.MTable
 import utils.MyPostgresDriver.simple._
+import java.sql.Timestamp
 
 object Global extends GlobalSettings {
   override def onStart(app: Application) {
@@ -21,6 +23,17 @@ object Global extends GlobalSettings {
           db._2.drop
       }
       ddl.create
+
+
+      val d = new java.util.Date()
+      val ts = new Timestamp(d.getTime)
+      // contests += Contest(1, ts, ts, "Contest 101", "Fayi", "This is the description", ts, ts, List(6))
+      val contestsInsert = contests ++= Seq(
+        Contest(1, ts, ts, "Goodbye 2013", "Fayi", "This is the description", ts, ts, List(1,2,3)),
+        Contest(2, ts, ts, "Welcome 2014", "Fayi", "This is the description", ts, ts, List(4,5)),
+        Contest(3, ts, ts, "Contest 101", "Fayi", "This is the description", ts, ts, List(6))
+      )
+
     }
   }
 }
