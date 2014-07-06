e3b941bc8603429faa2be2543aa2069b0e6425a0
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-06 13:00:28 +0100

    Cleanup Global. removed unused imports

--- app/Global.scala
+++ app/Global.scala
@@ -1,16 +1,8 @@
 import play.api.GlobalSettings
 
-import models._
-import models.database._
-import play.api.db.DB
 import play.api.Application
 import play.api.mvc.WithFilters
-import play.api.Play.current
-import scala.slick.jdbc.meta.MTable
-import utils.MyPostgresDriver.simple._
 import utils.CORSFilter
-import java.sql.Timestamp
-import play.Logger
 
 object Global extends WithFilters(CORSFilter()) with GlobalSettings {
 
