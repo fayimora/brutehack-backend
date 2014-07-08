134a101e3a41dd63d8209c2ac709a6159f2e392c
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-07-08 23:33:40 +0100

    Slight reshuffle to my evolutions.
    I KNOW this is not a good ides but I haven't deployed to a real prod server so it's totally fine!

--- app/models/database/TimestampedTable.scala
+++ app/models/database/TimestampedTable.scala
@@ -4,7 +4,7 @@ import utils.MyPostgresDriver.simple._
 import java.sql.Timestamp
 
 trait TimestampedTable {this: Table[_] =>
-  def createdAt = column[Timestamp]("CREATED_AT", O.NotNull)
-  def updatedAt = column[Timestamp]("UPDATED_AT", O.NotNull)
+  def createdAt = column[Timestamp]("CREATED_AT", O.Nullable)
+  def updatedAt = column[Timestamp]("UPDATED_AT", O.Nullable)
 }
 
--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -7,16 +7,10 @@ begin
 end;;
 $$ language 'plpgsql';
 
-alter table "CONTESTS" alter column "CREATED_AT" set default CURRENT_TIMESTAMP;
-alter table "CONTESTS" alter column "UPDATED_AT" set default CURRENT_TIMESTAMP;
 create trigger contests_timestamp_update BEFORE update on "CONTESTS" for each row execute procedure update_timestamp();
 
-alter table "PROBLEMS" alter column "CREATED_AT" set default CURRENT_TIMESTAMP;
-alter table "PROBLEMS" alter column "UPDATED_AT" set default CURRENT_TIMESTAMP;
 create trigger problems_timestamp_update before update on "PROBLEMS" for each row execute procedure update_timestamp();
 
-alter table "USERS" alter column "CREATED_AT" set default CURRENT_TIMESTAMP;
-alter table "USERS" alter column "UPDATED_AT" set default CURRENT_TIMESTAMP;
 create trigger users_timestamp_update before update on "USERS" for each row execute procedure update_timestamp();
 
 
@@ -24,15 +18,9 @@ create trigger users_timestamp_update before update on "USERS" for each row exec
 
 drop function update_timestamp() CASCADE;
 
-alter table "CONTESTS" alter column "CREATED_AT" drop default;
-alter table "CONTESTS" alter column "UPDATED_AT" drop default;
 drop trigger contests_timestamp_update on "CONTESTS";
 
-alter table "PROBLEMS" alter column "CREATED_AT" drop default;
-alter table "PROBLEMS" alter column "UPDATED_AT" drop default;
 drop trigger problems_timestamp_update on "PROBLEMS";
 
-alter table "USERS" alter column "CREATED_AT" drop default;
-alter table "USERS" alter column "UPDATED_AT" drop default;
 drop trigger users_timestamp_update on "USERS";
 
