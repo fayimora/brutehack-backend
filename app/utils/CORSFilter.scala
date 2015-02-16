8d43fbb7b30f7e7ad6f82f8f81bd7fcbca8ae04d
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-02-16 20:09:00 +0000

    Cleanup

--- app/utils/CORSFilter.scala
+++ app/utils/CORSFilter.scala
@@ -19,18 +19,18 @@ case class CORSFilter() extends Filter{
       Logger.trace("[cors] request is preflight")
       Logger.trace(s"[cors] default allowed domain is $allowedDomain")
       Future.successful(Default.Ok.withHeaders(
-        "Access-Control-Allow-Origin" -> allowedDomain.orElse(request.headers.get("Origin")).getOrElse(""),
-        "Access-Control-Allow-Methods" -> request.headers.get("Access-Control-Request-Method").getOrElse("*"),
-        "Access-Control-Allow-Headers" -> request.headers.get("Access-Control-Request-Headers").getOrElse(""),
+        "Access-Control-Allow-Origin"      -> allowedDomain.orElse(request.headers.get("Origin")).getOrElse(""),
+        "Access-Control-Allow-Methods"     -> request.headers.get("Access-Control-Request-Method").getOrElse("*"),
+        "Access-Control-Allow-Headers"     -> request.headers.get("Access-Control-Request-Headers").getOrElse(""),
         "Access-Control-Allow-Credentials" -> "true"
       ))
     } else {
       Logger.trace("[cors] request is normal")
       Logger.trace(s"[cors] default allowed domain is $allowedDomain")
       f(request).map{_.withHeaders(
-        "Access-Control-Allow-Origin" -> allowedDomain.orElse(request.headers.get("Origin")).getOrElse(""),
-        "Access-Control-Allow-Methods" -> request.headers.get("Access-Control-Request-Method").getOrElse("*"),
-        "Access-Control-Allow-Headers" -> request.headers.get("Access-Control-Request-Headers").getOrElse(""),
+        "Access-Control-Allow-Origin"      -> allowedDomain.orElse(request.headers.get("Origin")).getOrElse(""),
+        "Access-Control-Allow-Methods"     -> request.headers.get("Access-Control-Request-Method").getOrElse("*"),
+        "Access-Control-Allow-Headers"     -> request.headers.get("Access-Control-Request-Headers").getOrElse(""),
         "Access-Control-Allow-Credentials" -> "true"
       )}
     }
