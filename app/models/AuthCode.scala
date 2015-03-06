e74689403030a4bc0b190ac9a2328a9ea4eb5835
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-06 23:58:58 +0000

    Bug Fix: App is currently using userId not userGuid

--- app/models/AuthCode.scala
+++ app/models/AuthCode.scala
@@ -3,11 +3,11 @@ package models
 import play.api.db.DB
 import play.api.Play.current
 import utils.MyPostgresDriver.simple._
-import models.database.AuthCode
+import models.database.AuthCodes
 import java.sql.Timestamp
 
 case class AuthCode(authorizationCode: String,
-                    userGuid: UUID,
+                    userId: Long,
                     redirectUri: Option[String],
                     scope: Option[String],
                     clientId: Option[String],
