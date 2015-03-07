23e8301e9557b2c4998c653fd03c2a551a7888c9
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-07 02:39:45 +0000

    Add evolutions for authcodes and accesstokens

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -1,26 +1,30 @@
 # --- !Ups
 
-create or replace function update_timestamp() returns trigger as $$
-begin
-   NEW.updated_at = now();;
-   return NEW;;
-end;;
-$$ language 'plpgsql';
+create table access_tokens (
+  "token" VARCHAR(254) NOT NULL,
+  "refresh_token" VARCHAR(254) NOT NULL,
+  "client_id" VARCHAR(254) NOT NULL,
+  "user_id" BIGINT NOT NULL,
+  "scope" VARCHAR(254) NOT NULL,
+  "expires_in" BIGINT NOT NULL,
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "created_at" timestamp default current_timestamp,
+  "updated_at" timestamp default current_timestamp
+);
 
-create trigger contests_timestamp_update before update on contests for each row execute procedure update_timestamp();
-
-create trigger problems_timestamp_update before update on problems for each row execute procedure update_timestamp();
-
-create trigger users_timestamp_update before update on users for each row execute procedure update_timestamp();
-
+create table auth_codes (
+  "authorization_code" VARCHAR(254) NOT NULL PRIMARY KEY,
+  "user_id" BIGINT NOT NULL,
+  "redirect_uri" VARCHAR(254),
+  "scope" VARCHAR(254),
+  "client_id" VARCHAR(254),
+  "expires_in" INTEGER NOT NULL,
+  "created_at" TIMESTAMP,
+  "updated_at" TIMESTAMP
+);
 
 # --- !Downs
 
-drop function update_timestamp() cascade;
-
-drop trigger contests_timestamp_update on contests;
-
-drop trigger problems_timestamp_update on problems;
-
-drop trigger users_timestamp_update on users;
+drop table if exists access_tokens;
+drop table if exists auth_codes;
 
---
+++ conf/evolutions/default/6.sql
@@ -0,0 +1,29 @@
+# --- !Ups
+
+create or replace function update_timestamp() returns trigger as $$
+begin
+   NEW.updated_at = now();;
+   return NEW;;
+end;;
+$$ language 'plpgsql';
+
+create trigger contests_timestamp_update before update on contests for each row execute procedure update_timestamp();
+
+create trigger problems_timestamp_update before update on problems for each row execute procedure update_timestamp();
+
+create trigger users_timestamp_update before update on users for each row execute procedure update_timestamp();
+
+create trigger users_timestamp_update before update on access_tokens for each row execute procedure update_timestamp();
+
+create trigger users_timestamp_update before update on auth_codes for each row execute procedure update_timestamp();
+
+
+# --- !Downs
+
+drop function update_timestamp() cascade;
+drop trigger contests_timestamp_update on contests;
+drop trigger problems_timestamp_update on problems;
+drop trigger users_timestamp_update on users;
+drop trigger users_timestamp_update on access_tokens;
+drop trigger users_timestamp_update on auth_codes;
+
