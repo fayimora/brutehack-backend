d7ee719e3fa2709e97aa5f6e6ff206acedcd4a12
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-07 10:02:16 +0000

    Bug Fix: wrongly named db triggers

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -13,11 +13,11 @@ create trigger problems_timestamp_update before update on problems for each row 
 
 create trigger users_timestamp_update before update on users for each row execute procedure update_timestamp();
 
-create trigger users_timestamp_update before update on access_tokens for each row execute procedure update_timestamp();
+create trigger access_tokens_timestamp_update before update on access_tokens for each row execute procedure update_timestamp();
 
-create trigger users_timestamp_update before update on auth_codes for each row execute procedure update_timestamp();
+create trigger auth_codes_timestamp_update before update on auth_codes for each row execute procedure update_timestamp();
 
-create trigger users_timestamp_update before update on clients for each row execute procedure update_timestamp();
+create trigger clients_timestamp_update before update on clients for each row execute procedure update_timestamp();
 
 
 # --- !Downs
@@ -26,7 +26,7 @@ drop function update_timestamp() cascade;
 drop trigger contests_timestamp_update on contests;
 drop trigger problems_timestamp_update on problems;
 drop trigger users_timestamp_update on users;
-drop trigger users_timestamp_update on access_tokens;
-drop trigger users_timestamp_update on auth_codes;
-drop trigger users_timestamp_update on clients;
+drop trigger access_tokens_timestamp_update on access_tokens;
+drop trigger auth_codes_timestamp_update on auth_codes;
+drop trigger clients_timestamp_update on clients;
 
