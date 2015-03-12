a1a43bda3f527feb194c0ac0412848ff506da70b
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-12 11:29:08 +0000

    Implement findUser() for OAuth2DataHandler

--- app/oauth2/OAuth2DataHandler.scala
+++ app/oauth2/OAuth2DataHandler.scala
@@ -3,11 +3,19 @@ package oauth2
 import scalaoauth2.provider.{DataHandler, AuthInfo, ClientCredential, AccessToken => AT}
 import scala.concurrent.{Future, ExecutionContext}
 import models.{User, AccessToken}
+import dao.UsersDAO
+import scala.util.{Try, Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class OAuth2DataHandler extends DataHandler[User] {
   def validateClient(clientCredential: ClientCredential, grantType: String): Future[Boolean] = ???
 
-  def findUser(username: String, password: String): Future[Option[User]] = ???
+  def findUser(username: String, password: String): Future[Option[User]] = Future {
+    UsersDAO.findByHandle(username) match {
+      case Success(user) => user
+      case Failure(err) => None
+    }
+  }
 
   def createAccessToken(authInfo: AuthInfo[User]): Future[AT] = ???
 
