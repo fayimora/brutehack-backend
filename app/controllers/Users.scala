8fbd027a4cee4f22ec68333ded78ade96a26f3c4
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-24 11:21:02 +0000

    Create "subtypes" of User.
    We don't want to return fields like `password` to the client so use helper UserProfile class to do
    that.

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import models.User
+import models.{User, UserProfile}
 import dao.UserDAO
 import scala.util.{Success, Failure}
 import java.sql.Timestamp
@@ -14,20 +14,18 @@ object Users extends Controller {
 
   implicit val timeReads: Reads[Timestamp] = (__ \ "time").read[Long].map{ long => new Timestamp(long) }
 
-  implicit val userWrites: Writes[User] = (
+  implicit val userProfileWrites: Writes[UserProfile] = (
     (__ \ "lastVisit").write[Timestamp] and
     (__ \ "handle").write[String] and
     (__ \ "firstName").write[String] and
     (__ \ "lastName").write[String] and
     (__ \ "email").write[String] and
-    (__ \ "password").write[String] and
     (__ \ "rating").write[Int] and
     (__ \ "location").write[String] and
     (__ \ "shirtSize").write[String] and
     (__ \ "id").writeNullable[Long] and
-    (__ \ "createdAt").writeNullable[Timestamp] and
-    (__ \ "updatedAt").writeNullable[Timestamp]
-  )(unlift(User.unapply))
+    (__ \ "createdAt").writeNullable[Timestamp]
+  )(unlift(UserProfile.unapply))
 
   implicit val userReads: Reads[User] = (
     (__ \ "lastVisit").readNullable[Timestamp].map(_.getOrElse(now)) and
@@ -44,21 +42,21 @@ object Users extends Controller {
     (__ \ "updatedAt").readNullable[Timestamp].map(_.orElse(Option(now)))
   )(User.apply _)
 
-  implicit val userFormat: Format[User] = Format(userReads, userWrites)
-
 
   def now = new Timestamp(System.currentTimeMillis())
 
   def index = Action.async {
     UserDAO.list.map{ usersTry =>
-      usersTry.map(users => Ok(Json.toJson(users))).getOrElse(InternalServerError)
+      usersTry.map(users =>
+          Ok(Json.toJson(users.map(_.profile)))
+      ).getOrElse(InternalServerError)
     }
   }
 
   def show(handle: String) = Action.async {
     UserDAO.findByHandle(handle).map {
       case Success(userOpt) => userOpt match {
-        case Some(user) => Ok(Json.obj("user" -> Json.toJson(user)))
+        case Some(user) => Ok(Json.obj("user" -> Json.toJson(user.profile)))
         case None => NotFound(Json.obj("error" -> s"$handle was not found"))
       }
       case Failure(err) => InternalServerError
@@ -72,7 +70,7 @@ object Users extends Controller {
       val (encryptedPass, salt) = oauth2.Crypto.encryptPassword(newUser.password)
       val encryptedUser = newUser.copy(password = encryptedPass)
       UserDAO.create(encryptedUser).map {
-        case Success(u) => Ok(Json.toJson(u))
+        case Success(u) => Ok(Json.toJson(u.profile))
         case Failure(err) => BadRequest(err.getMessage)
       }
     }.getOrElse(Future.successful(BadRequest("Invalid Json")))
--- app/models/User.scala
+++ app/models/User.scala
@@ -6,6 +6,19 @@ import utils.MyPostgresDriver.simple._
 import models.database.Users
 import java.sql.Timestamp
 
+case class UserResponse(id: Option[Long], handle: String, email: String, rating: Int)
+
+case class UserProfile(lastVisit: Timestamp,
+                handle: String,
+                firstName: String,
+                lastName: String,
+                email: String,
+                rating: Int,
+                location: String,
+                shirtSize: String,
+                id: Option[Long],
+                createdAt: Option[Timestamp])
+
 case class User(lastVisit: Timestamp,
                 handle: String,
                 firstName: String,
@@ -17,5 +30,12 @@ case class User(lastVisit: Timestamp,
                 shirtSize: String,
                 id: Option[Long],
                 createdAt: Option[Timestamp],
-                updatedAt: Option[Timestamp])
+                updatedAt: Option[Timestamp]) {
+
+  def signupResponse =
+    UserResponse(id, handle, email, rating)
+
+  def profile =
+    UserProfile(lastVisit, handle, firstName, lastName, email, rating, location, shirtSize, id, createdAt)
+}
 
