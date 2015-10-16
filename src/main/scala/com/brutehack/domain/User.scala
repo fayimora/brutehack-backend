09402cbefb111c24e31ec488e65fbe5bbec5e5c5
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-10-16 14:02:21 +0100

    Add User domain

---
+++ src/main/scala/com/brutehack/domain/User.scala
@@ -0,0 +1,14 @@
+package com.brutehack.domain
+
+/**
+ * Created by fayimora on 16/10/2015.
+ */
+case class User(id: String,
+                handle: String,
+                firstName: String,
+                lastName: String,
+                email: String,
+                password: String,
+                rating: Int,
+                location: String,
+                shirtSize: String)
