5d34eacc2d7c5eca0966724c7b00fc0bf4d7654c
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-10-18 13:21:44 +0100

    Add util for hashing passwords

---
+++ src/main/scala/com/brutehack/Crypto.scala
@@ -0,0 +1,20 @@
+package com.brutehack
+
+import com.twitter.inject.Logging
+import org.mindrot.jbcrypt.BCrypt
+
+/**
+ * Created by fayimora on 18/10/2015.
+ */
+object Crypto extends Logging {
+
+  def encryptPassword(password: String): (String, String) = {
+    debug("Generating password Salt")
+    val salt = BCrypt.gensalt(15)
+    debug("Salt generated")
+    val hash = BCrypt.hashpw(password, salt)
+    debug("Password hashed")
+    (hash, salt)
+  }
+
+}
