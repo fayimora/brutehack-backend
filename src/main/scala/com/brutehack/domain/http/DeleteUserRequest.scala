02ad1874c4867bc083160601db50a87b48ebdf2a
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-11-01 21:14:34 +0000

    Validate handle on DELETE requests

--- src/main/scala/com/brutehack/domain/http/DeleteUserRequest.scala
+++ src/main/scala/com/brutehack/domain/http/DeleteUserRequest.scala
@@ -1,6 +1,8 @@
 package com.brutehack.domain.http
 
+import com.twitter.finatra.validation.NotEmpty
+
 /**
  * Created by fayimora on 18/10/2015.
  */
-case class DeleteUserRequest(handle: String)
+case class DeleteUserRequest(@NotEmpty handle: String)
