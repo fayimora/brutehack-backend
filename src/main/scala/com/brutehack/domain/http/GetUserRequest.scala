e8fa9fa44dc5c347b3ea7eff64d8c838113b7ea2
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-11-04 22:22:50 +0000

    TODO

--- src/main/scala/com/brutehack/domain/http/GetUserRequest.scala
+++ src/main/scala/com/brutehack/domain/http/GetUserRequest.scala
@@ -1,9 +1,15 @@
 package com.brutehack.domain.http
 
 import com.twitter.finatra.request.RouteParam
-import com.twitter.finatra.validation.NotEmpty
+import com.twitter.finatra.validation.{ValidationResult, MethodValidation, NotEmpty}
 
 /**
  * Created by fayimora on 18/10/2015.
  */
-case class GetUserRequest(@RouteParam @NotEmpty handle: String)
+case class GetUserRequest(@RouteParam @NotEmpty handle: String) {
+  // TODO: make sure handle exists
+  @MethodValidation
+  def handleValid() = {
+    ValidationResult(true, s"$handle does not exist")
+  }
+}
