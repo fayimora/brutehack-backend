67303d77316e454b4495f7bf23424b362340bdfd
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-12-08 01:02:13 +0000

    Add rating to user response

--- src/main/scala/com/brutehack/domain/http/UserResponse.scala
+++ src/main/scala/com/brutehack/domain/http/UserResponse.scala
@@ -5,8 +5,8 @@ import com.brutehack.domain.User
 /**
  * Created by fayimora on 16/10/2015.
  */
-case class UserResponse(id: String, handle: String, email: String)
+case class UserResponse(id: String, handle: String, email: String, rating: Int)
 
 object UserResponse {
-  def fromDomain(u: User) = UserResponse(u.id, u.handle, u.email)
+  def fromDomain(u: User) = UserResponse(u.id, u.handle, u.email, u.rating)
 }
